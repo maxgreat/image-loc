0789353e110896f284465725bcc28166227ecb79
Maxime Portaz <maxime.portaz@gmail.com>
2019-06-11 15:27:37 +0000

    add test.py

---
+++ main_test.py
@@ -0,0 +1,368 @@
+from __future__ import print_function
+import argparse
+from math import log10, ceil
+import random, shutil, json
+from os.path import join, exists, isfile, realpath, dirname
+from os import makedirs, remove, chdir, environ
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from torch.utils.data.dataset import Subset
+import torchvision.transforms as transforms
+from PIL import Image
+from datetime import datetime
+import torchvision.datasets as datasets
+import torchvision.models as models
+import h5py
+import faiss
+
+from tensorboardX import SummaryWriter
+import numpy as np
+import model
+
+
+def train(epoch):
+    epoch_loss = 0
+    startIter = 1 # keep track of batch iter across subsets for logging
+
+    if opt.cacheRefreshRate > 0:
+        subsetN = ceil(len(train_set) / opt.cacheRefreshRate)
+        #TODO randomise the arange before splitting?
+        subsetIdx = np.array_split(np.arange(len(train_set)), subsetN)
+    else:
+        subsetN = 1
+        subsetIdx = [np.arange(len(train_set))]
+
+    nBatches = (len(train_set) + opt.batchSize - 1) // opt.batchSize
+
+    for subIter in range(subsetN):
+        print('====> Building Cache')
+        model.eval()
+        train_set.cache = join(opt.cachePath, train_set.whichSet + '_feat_cache.hdf5')
+        with h5py.File(train_set.cache, mode='w') as h5: 
+            pool_size = encoder_dim
+            if opt.pooling.lower() == 'netvlad': pool_size *= opt.num_clusters
+            h5feat = h5.create_dataset("features", 
+                    [len(whole_train_set), pool_size], 
+                    dtype=np.float32)
+            with torch.no_grad():
+                for iteration, (input, indices) in enumerate(whole_training_data_loader, 1):
+                    input = input.to(device)
+                    image_encoding = model.encoder(input)
+                    vlad_encoding = model.pool(image_encoding) 
+                    h5feat[indices.detach().numpy(), :] = vlad_encoding.detach().cpu().numpy()
+                    del input, image_encoding, vlad_encoding
+
+        sub_train_set = Subset(dataset=train_set, indices=subsetIdx[subIter])
+
+        training_data_loader = DataLoader(dataset=sub_train_set, num_workers=opt.threads, 
+                    batch_size=opt.batchSize, shuffle=True, 
+                    collate_fn=dataset.collate_fn, pin_memory=cuda)
+
+        print('Allocated:', torch.cuda.memory_allocated())
+        print('Cached:', torch.cuda.memory_cached())
+
+        model.train()
+        for iteration, (query, positives, negatives, 
+                negCounts, indices) in enumerate(training_data_loader, startIter):
+            # some reshaping to put query, pos, negs in a single (N, 3, H, W) tensor
+            # where N = batchSize * (nQuery + nPos + nNeg)
+            if query is None: continue # in case we get an empty batch
+
+            B, C, H, W = query.shape
+            nNeg = torch.sum(negCounts)
+            input = torch.cat([query, positives, negatives])
+
+            input = input.to(device)
+            image_encoding = model.encoder(input)
+            vlad_encoding = model.pool(image_encoding) 
+
+            vladQ, vladP, vladN = torch.split(vlad_encoding, [B, B, nNeg])
+
+            optimizer.zero_grad()
+            
+            # calculate loss for each Query, Positive, Negative triplet
+            # due to potential difference in number of negatives have to 
+            # do it per query, per negative
+            loss = 0
+            for i, negCount in enumerate(negCounts):
+                for n in range(negCount):
+                    negIx = (torch.sum(negCounts[:i]) + n).item()
+                    loss += criterion(vladQ[i:i+1], vladP[i:i+1], vladN[negIx:negIx+1])
+
+            loss /= nNeg.float().to(device) # normalise by actual number of negatives
+            loss.backward()
+            optimizer.step()
+            del input, image_encoding, vlad_encoding, vladQ, vladP, vladN
+            del query, positives, negatives
+
+            batch_loss = loss.item()
+            epoch_loss += batch_loss
+
+            if iteration % 50 == 0 or nBatches <= 10:
+                print("==> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, 
+                    nBatches, batch_loss), flush=True)
+                writer.add_scalar('Train/Loss', batch_loss, 
+                        ((epoch-1) * nBatches) + iteration)
+                writer.add_scalar('Train/nNeg', nNeg, 
+                        ((epoch-1) * nBatches) + iteration)
+                print('Allocated:', torch.cuda.memory_allocated())
+                print('Cached:', torch.cuda.memory_cached())
+
+        startIter += len(training_data_loader)
+        del training_data_loader, loss
+        optimizer.zero_grad()
+        torch.cuda.empty_cache()
+        remove(train_set.cache) # delete HDF5 cache
+
+    avg_loss = epoch_loss / nBatches
+
+    print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, avg_loss), 
+            flush=True)
+    writer.add_scalar('Train/AvgLoss', avg_loss, epoch)
+
+def test(eval_set, epoch=0, write_tboard=False):
+    # TODO what if features dont fit in memory? 
+    test_data_loader = DataLoader(dataset=eval_set, 
+                num_workers=opt.threads, batch_size=opt.cacheBatchSize, shuffle=False, 
+                pin_memory=cuda)
+
+    model.eval()
+    with torch.no_grad():
+        print('====> Extracting Features')
+        pool_size = encoder_dim
+        if opt.pooling.lower() == 'netvlad': pool_size *= opt.num_clusters
+        dbFeat = np.empty((len(eval_set), pool_size))
+
+        for iteration, (input, indices) in enumerate(test_data_loader, 1):
+            input = input.to(device)
+            image_encoding = model.encoder(input)
+            vlad_encoding = model.pool(image_encoding) 
+
+            dbFeat[indices.detach().numpy(), :] = vlad_encoding.detach().cpu().numpy()
+            if iteration % 50 == 0 or len(test_data_loader) <= 10:
+                print("==> Batch ({}/{})".format(iteration, 
+                    len(test_data_loader)), flush=True)
+
+            del input, image_encoding, vlad_encoding
+    del test_data_loader
+
+    # extracted for both db and query, now split in own sets
+    qFeat = dbFeat[eval_set.dbStruct.numDb:].astype('float32')
+    dbFeat = dbFeat[:eval_set.dbStruct.numDb].astype('float32')
+    
+    print('====> Building faiss index')
+    faiss_index = faiss.IndexFlatL2(pool_size)
+    faiss_index.add(dbFeat)
+
+    print('====> Calculating recall @ N')
+    n_values = [1,5,10,20]
+
+    _, predictions = faiss_index.search(qFeat, max(n_values)) 
+
+    # for each query get those within threshold distance
+    gt = eval_set.getPositives() 
+
+    correct_at_n = np.zeros(len(n_values))
+    #TODO can we do this on the matrix in one go?
+    for qIx, pred in enumerate(predictions):
+        for i,n in enumerate(n_values):
+            # if in top N then also in top NN, where NN > N
+            if np.any(np.in1d(pred[:n], gt[qIx])):
+                correct_at_n[i:] += 1
+                break
+    recall_at_n = correct_at_n / eval_set.dbStruct.numQ
+
+    recalls = {} #make dict for output
+    for i,n in enumerate(n_values):
+        recalls[n] = recall_at_n[i]
+        print("====> Recall@{}: {:.4f}".format(n, recall_at_n[i]))
+        if write_tboard: writer.add_scalar('Val/Recall@' + str(n), recall_at_n[i], epoch)
+
+    return recalls
+
+def get_clusters(cluster_set):
+    nDescriptors = 50000
+    nPerImage = 100
+    nIm = ceil(nDescriptors/nPerImage)
+
+    sampler = SubsetRandomSampler(np.random.choice(len(cluster_set), nIm, replace=False))
+    data_loader = DataLoader(dataset=cluster_set, 
+                num_workers=opt.threads, batch_size=opt.cacheBatchSize, shuffle=False, 
+                pin_memory=cuda,
+                sampler=sampler)
+
+    if not exists(join(opt.dataPath, 'centroids')):
+        makedirs(join(opt.dataPath, 'centroids'))
+
+    initcache = join(opt.dataPath, 'centroids', opt.arch + '_' + cluster_set.dataset + '_' + str(opt.num_clusters) + '_desc_cen.hdf5')
+    with h5py.File(initcache, mode='w') as h5: 
+        with torch.no_grad():
+            model.eval()
+            print('====> Extracting Descriptors')
+            dbFeat = h5.create_dataset("descriptors", 
+                        [nDescriptors, encoder_dim], 
+                        dtype=np.float32)
+
+            for iteration, (input, indices) in enumerate(data_loader, 1):
+                input = input.to(device)
+                image_descriptors = model.encoder(input).view(input.size(0), encoder_dim, -1).permute(0, 2, 1)
+
+                batchix = (iteration-1)*opt.cacheBatchSize*nPerImage
+                for ix in range(image_descriptors.size(0)):
+                    # sample different location for each image in batch
+                    sample = np.random.choice(image_descriptors.size(1), nPerImage, replace=False)
+                    startix = batchix + ix*nPerImage
+                    dbFeat[startix:startix+nPerImage, :] = image_descriptors[ix, sample, :].detach().cpu().numpy()
+
+                if iteration % 50 == 0 or len(data_loader) <= 10:
+                    print("==> Batch ({}/{})".format(iteration, 
+                        ceil(nIm/opt.cacheBatchSize)), flush=True)
+                del input, image_descriptors
+        
+        print('====> Clustering..')
+        niter = 100
+        kmeans = faiss.Kmeans(encoder_dim, opt.num_clusters, niter, verbose=False)
+        kmeans.train(dbFeat[...])
+
+        print('====> Storing centroids', kmeans.centroids.shape)
+        h5.create_dataset('centroids', data=kmeans.centroids)
+        print('====> Done!')
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    model_out_path = join(opt.savePath, filename)
+    torch.save(state, model_out_path)
+    if is_best:
+        shutil.copyfile(model_out_path, join(opt.savePath, 'model_best.pth.tar'))
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+class L2Norm(nn.Module):
+    def __init__(self, dim=1):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, input):
+        return F.normalize(input, p=2, dim=self.dim)
+
+if __name__ == "__main__":
+    encoder_dim = 512
+    encoder = models.vgg16(pretrained=pretrained)
+    # capture only feature part and remove last relu and maxpool
+    layers = list(encoder.features.children())[:-2]
+    
+    net_vlad = netvlad.NetVLAD(num_clusters=opt.num_clusters, dim=encoder_dim, vladv2=False)
+    layers.append(net_vlad)
+    encoder = nn.Sequential(*layers)
+    model = nn.Module() 
+    model.add_module('encoder', encoder)
+
+    """
+    elif opt.pooling.lower() == 'max':
+        global_pool = nn.AdaptiveMaxPool2d((1,1))
+        model.add_module('pool', nn.Sequential(*[global_pool, Flatten(), L2Norm()]))
+    elif opt.pooling.lower() == 'avg':
+        global_pool = nn.AdaptiveAvgPool2d((1,1))
+        model.add_module('pool', nn.Sequential(*[global_pool, Flatten(), L2Norm()]))
+    else:
+        raise ValueError('Unknown pooling type: ' + opt.pooling)
+    """
+    
+    
+    if opt.mode.lower() == 'train':
+        if opt.optim.upper() == 'ADAM':
+            optimizer = optim.Adam(filter(lambda p: p.requires_grad, 
+                model.parameters()), lr=opt.lr)#, betas=(0,0.9))
+        elif opt.optim.upper() == 'SGD':
+            optimizer = optim.SGD(filter(lambda p: p.requires_grad, 
+                model.parameters()), lr=opt.lr,
+                momentum=opt.momentum,
+                weight_decay=opt.weightDecay)
+
+            scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=opt.lrStep, gamma=opt.lrGamma)
+        else:
+            raise ValueError('Unknown optimizer: ' + opt.optim)
+
+        # original paper/code doesn't sqrt() the distances, we do, so sqrt() the margin, I think :D
+        criterion = nn.TripletMarginLoss(margin=opt.margin**0.5, 
+                p=2, reduction='sum').to(device)
+
+    if opt.resume:
+        if opt.ckpt.lower() == 'latest':
+            resume_ckpt = join(opt.resume, 'checkpoints', 'checkpoint.pth.tar')
+        elif opt.ckpt.lower() == 'best':
+            resume_ckpt = join(opt.resume, 'checkpoints', 'model_best.pth.tar')
+
+        if isfile(resume_ckpt):
+            print("=> loading checkpoint '{}'".format(resume_ckpt))
+            checkpoint = torch.load(resume_ckpt, map_location=lambda storage, loc: storage)
+            opt.start_epoch = checkpoint['epoch']
+            best_metric = checkpoint['best_score']
+            model.load_state_dict(checkpoint['state_dict'])
+            model = model.to(device)
+            if opt.mode == 'train':
+                optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(resume_ckpt, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(resume_ckpt))
+
+    if opt.mode.lower() == 'test':
+        print('===> Running evaluation step')
+        epoch = 1
+        recalls = test(whole_test_set, epoch, write_tboard=False)
+    elif opt.mode.lower() == 'cluster':
+        print('===> Calculating descriptors and clusters')
+        get_clusters(whole_train_set)
+    elif opt.mode.lower() == 'train':
+        print('===> Training model')
+        writer = SummaryWriter(log_dir=join(opt.runsPath, datetime.now().strftime('%b%d_%H-%M-%S')+'_'+opt.arch+'_'+opt.pooling))
+
+        # write checkpoints in logdir
+        logdir = writer.file_writer.get_logdir()
+        opt.savePath = join(logdir, opt.savePath)
+        if not opt.resume:
+            makedirs(opt.savePath)
+
+        with open(join(opt.savePath, 'flags.json'), 'w') as f:
+            f.write(json.dumps(
+                {k:v for k,v in vars(opt).items()}
+                ))
+        print('===> Saving state to:', logdir)
+
+        not_improved = 0
+        best_score = 0
+        for epoch in range(opt.start_epoch+1, opt.nEpochs + 1):
+            if opt.optim.upper() == 'SGD':
+                scheduler.step(epoch)
+            train(epoch)
+            if (epoch % opt.evalEvery) == 0:
+                recalls = test(whole_test_set, epoch, write_tboard=True)
+                is_best = recalls[5] > best_score 
+                if is_best:
+                    not_improved = 0
+                    best_score = recalls[5]
+                else: 
+                    not_improved += 1
+
+                save_checkpoint({
+                        'epoch': epoch,
+                        'state_dict': model.state_dict(),
+                        'recalls': recalls,
+                        'best_score': best_score,
+                        'optimizer' : optimizer.state_dict(),
+                        'parallel' : isParallel,
+                }, is_best)
+
+                if opt.patience > 0 and not_improved > (opt.patience / opt.evalEvery):
+                    print('Performance did not improve for', opt.patience, 'epochs. Stopping.')
+                    break
+
+        print("=> Best Recall@5: {:.4f}".format(best_score), flush=True)
+        writer.close()
